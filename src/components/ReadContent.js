509ec0a73cb0140c48d02d7c75ae962e7ad4400e
dimorin <dimorinpk@gmail.com>
2022-01-14 22:51:07 +0900

    first commit

---
+++ src/components/ReadContent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ReadContent extends Component {
+    render(){
+      return (
+        <article>
+            <h2>{this.props.title}</h2>
+            <p>{this.props.desc}</p>
+            
+        </article>
+      );
+    }
+  }
+
+  export default ReadContent;
